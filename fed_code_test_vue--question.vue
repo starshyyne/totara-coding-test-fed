93e542da16a7cd37026b5d1cb8a598dee04530ff
Laura Munro <munro.lauraanne@gmail.com>
2020-11-17 12:38:32 +1300

    Init commit add original files

---
+++ fed_code_test_vue--question.vue
@@ -0,0 +1,20 @@
+<!--
+
+The task: Correctly arrange component data into suitable part of the Vue API
+
+Properties:
+ - Data, named `id`, that is a static String and has a value of `feature`
+ - Data, named `list`, that is received from a parent component and has a default value
+ - Data, named `description`, that is a String in the format "This list contains x item(s).", where `x` is equal to the number of `list` items provided to this component.
+-->
+
+<template>
+  <div>
+    {{ description }}
+  </div>
+</template>
+<script>
+	export default {
+
+	};
+</script>
